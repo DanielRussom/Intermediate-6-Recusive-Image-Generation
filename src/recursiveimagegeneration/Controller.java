9528604412a89b7912ad4ff414b85d713f55d123
Daniel Russom <daniel.russom00@gmail.com>
2018-01-02 21:17:25 +0000

    First Line
    -GUI Added
    -Single line drawn

--- src/recursiveimagegeneration/Controller.java
+++ src/recursiveimagegeneration/Controller.java
@@ -1,10 +1,24 @@
 package recursiveimagegeneration;
 
-public class Controller {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JApplet;
 
+public class Controller extends JApplet {
+
+	public void paint() {
+		setBackground(new Color(255, 0, 0));
 	}
 
+	public static void main(String[] args) {
+	}
+
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.draw(new Line2D.Double(0,0,100,100));
+	}
 }
