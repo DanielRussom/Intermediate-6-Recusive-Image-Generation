0fb86ff1287d1f3cfb32e04a94ca1da09bc72408
Daniel Russom <daniel.russom00@gmail.com>
2018-01-02 21:37:41 +0000

    Rectangle Test
    -Added hard coded display of rectangles
    -Removed line test

--- src/recursiveimagegeneration/Controller.java
+++ src/recursiveimagegeneration/Controller.java
@@ -4,11 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JApplet;
 
 public class Controller extends JApplet {
-
+	int startingPosition = 0;
+	int totalWidth = 100;
+	int totalHeight = 50;
+	int depth = 1;
+	
 	public void paint() {
 		setBackground(new Color(255, 0, 0));
 	}
@@ -19,6 +24,10 @@ public class Controller extends JApplet {
 	
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		g2.draw(new Line2D.Double(0,0,100,100));
+		g2.draw(new Rectangle2D.Double(0, 0, 100, 50));
+		g2.draw(new Rectangle2D.Double(0, 0, 50, 25));
+		g2.draw(new Rectangle2D.Double(50, 0, 50, 25));
+		g2.draw(new Rectangle2D.Double(0, 25, 50, 25));
+		g2.draw(new Rectangle2D.Double(50, 25, 50, 25));
 	}
 }
