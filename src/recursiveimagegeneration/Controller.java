33c1b5de2ee209c8a11f135e2af4a7097bd8c397
Daniel Russom <daniel.russom00@gmail.com>
2018-01-03 17:58:57 +0000

    Recursive Function
    -Function now calls recursively to set limit
    -Triangles are dynamically generated
    -Middle triangle being generated within, instead of three outer triangles.

--- src/recursiveimagegeneration/Controller.java
+++ src/recursiveimagegeneration/Controller.java
@@ -11,24 +11,41 @@ public class Controller extends JApplet {
 	int startingPosition = 0;
 	int totalWidth = 100;
 	int totalHeight = 50;
-		
+
 	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		// Draws a triangle
-		int[] xCoords = new int[] {0,380,190};
-		int[] yCoords = new int[] {380,380,0};
+		int[] xCoords = new int[] { 0, 380, 190 };
+		int[] yCoords = new int[] { 380, 380, 0 };
 		g2.drawPolygon(xCoords, yCoords, 3);
-		drawTriangle(3, null, null, null, g2);
-		
+		drawTriangle(3, xCoords, yCoords, g2);
+
 	}
-	
-	public void drawTriangle(int steps, Point coord1, Point coord2, Point coord3, Graphics2D g2) {
-		Point intersect1 = new Point((int)(coord1.getX()+coord2.getX())/2,(int)(coord1.getY()+coord2.getY())/2);
-		Point intersect2 = new Point((0+190)/2, (380+0)/2);
-		Point intersect3 = new Point((380+190)/2, (380+0)/2);
-		
-		g2.draw(new Line2D.Double(intersect1.getX(), intersect1.getY(), intersect2.getX(), intersect2.getY()));
-		g2.draw(new Line2D.Double(intersect2.getX(), intersect2.getY(), intersect3.getX(), intersect3.getY()));
-		g2.draw(new Line2D.Double(intersect1.getX(), intersect1.getY(), intersect3.getX(), intersect3.getY()));
+
+	public void drawTriangle(int steps, int[] xCoords, int[] yCoords, Graphics2D g2) {
+		int[] newXCoords = new int[3];
+		int[] newYCoords = new int[3];
+		newXCoords[0] = (xCoords[0] + xCoords[1]) / 2;
+		newYCoords[0] = (yCoords[0] + yCoords[1]) / 2;
+		newXCoords[1] = (xCoords[0] + xCoords[2]) / 2;
+		newYCoords[1] = (yCoords[0] + yCoords[2]) / 2;
+		newXCoords[2] = (xCoords[1] + xCoords[2]) / 2;
+		newYCoords[2] = (yCoords[1] + yCoords[2]) / 2;
+		Point intersect1 = new Point((xCoords[0] + xCoords[1]) / 2, (yCoords[0] + yCoords[1]) / 2);
+		Point intersect2 = new Point((xCoords[1] + xCoords[2]) / 2, (yCoords[1] + yCoords[2]) / 2);
+		Point intersect3 = new Point((xCoords[0] + xCoords[2]) / 2, (yCoords[0] + yCoords[2]) / 2);
+		g2.drawPolygon(newXCoords, newYCoords, 3);
+		if (steps > 1) {
+			drawTriangle(steps - 1, newXCoords, newYCoords, g2);
+		} else {
+			return;
+		}
+		//
+		// g2.draw(new Line2D.Double(intersect1.getX(), intersect1.getY(),
+		// intersect2.getX(), intersect2.getY()));
+		// g2.draw(new Line2D.Double(intersect2.getX(), intersect2.getY(),
+		// intersect3.getX(), intersect3.getY()));
+		// g2.draw(new Line2D.Double(intersect1.getX(), intersect1.getY(),
+		// intersect3.getX(), intersect3.getY()));
 	}
 }
